5ade3465503ad70ea68aa9f4df5157f8f724cf21
Fuad Saud <fuadfsaud@gmail.com>
2015-12-27 02:49:25 -0200

    Day 4: Complete solution

--- day4/Day4.hs
+++ day4/Day4.hs
@@ -1,6 +1,7 @@
 module Day4 where
 
 import Data.List
+import Control.Monad
 import qualified Crypto.Hash.MD5 as MD5
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C8
@@ -21,7 +22,13 @@ startsWithZeroes n = BS.isPrefixOf (C8.pack $ replicate n '0') . B16.encode
 message :: String -> Int -> Message
 message key i = C8.pack $ key ++ show i
 
+untilSolution :: Int -> Int
+untilSolution nZeroes = until (startsWithZeroes nZeroes . md5 . message secret) (+1) 1
+
+findSolution :: Int -> Maybe Int
+findSolution nZeroes = fmap (+1) . findIndex (startsWithZeroes nZeroes) . map (md5 . message secret) $ [1..]
+
 main :: IO ()
 main = do
-    print $ until (startsWithZeroes 5 . md5 . message secret) (+1) 1
-
+    forM_ [5, 6] $ \i -> print $ untilSolution i
+    forM_ [5, 6] $ \i -> print $ findSolution i
