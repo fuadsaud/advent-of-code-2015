968092177b7529a838489fa2251b28084524cba2
Fuad Saud <fuadfsaud@gmail.com>
2015-12-27 02:32:28 -0200

    Day 2: Add solution for part 2

--- day2/Day2.hs
+++ day2/Day2.hs
@@ -1,10 +1,13 @@
 module Day2 where
 
-import Data.List.Split
+import Data.List.Split (splitOn)
+import Data.List (sort)
 
 data Box = Box Integer Integer Integer deriving Show
 
 makeBox (l:w:h:[]) = Box l w h
+
+dimensions (Box l w h) = [l, w, h]
 
 inputFileName = "input"
 
@@ -14,17 +17,28 @@ instance Read Box where
 surface :: Box -> Integer
 surface (Box l w h) = (2 * l * w) + (2 * w * h) + (2 * h * l)
 
-wrappingPaperSize :: Box -> Integer
-wrappingPaperSize box@(Box l w h) = smallerSize + surface box
+wrappingPaperArea :: Box -> Integer
+wrappingPaperArea box@(Box l w h) = smallerSize + surface box
   where
     smallerSize = minimum [l * w, w * h, h * l]
 
-totalArea :: [Box] -> Integer
-totalArea = foldl (+) 0 . map wrappingPaperSize
+ribbonArea :: Box -> Integer
+ribbonArea box = ribbonWrapArea + ribbonBowArea
+  where
+      dims = dimensions box
+      ribbonWrapArea = sum . map (*2) . take 2 . sort $ dims
+      ribbonBowArea = product dims
+
+totalWrappingPaperArea :: [Box] -> Integer
+totalWrappingPaperArea = sum . map wrappingPaperArea
+
+totalRibbonArea :: [Box] -> Integer
+totalRibbonArea = sum . map ribbonArea
 
 main = do
     input <- readFile inputFileName
 
     let boxes = map read . lines $ input
 
-    print $ totalArea boxes
+    print $ totalWrappingPaperArea boxes
+    print $ totalRibbonArea boxes
